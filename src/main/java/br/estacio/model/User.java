599a716443da92c2c09d0e3de9f8a82b8dd4e6d1
Aluno <aluno@estacio.acad>
2017-09-22 18:17:11 -0300

    Aula 6 imcompleta

---
+++ src/main/java/br/estacio/model/User.java
@@ -0,0 +1,80 @@
+package br.estacio.model;
+
+import java.security.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Version;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.Email;
+
+@Entity
+public class User {
+	
+	@Id
+	@GeneratedValue
+	private long id;
+	
+	@Size(min = 10, message = "Entre pelo menos 10 caracteres.")
+	@Column(length=60, nullable=false)
+	private String name;
+	
+	@Email
+	@Column(length=60, nullable=false)
+	private String email;
+	
+	@Pattern(regexp = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$", message = "Min 8 com maúscula, minúsculas,letras, números e caraceteres.")
+	@Column(length=60, nullable=false)
+	private String pwd;
+	
+	@NotNull
+	@Column(length = 10, nullable = false)
+	private String Estado;
+	
+	@Version
+	@Column(nullable = false)
+	protected Timestamp timestamp;
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", email=" + email + ", pwd=" + pwd + "]";
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPwd() {
+		return pwd;
+	}
+
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+
+}
